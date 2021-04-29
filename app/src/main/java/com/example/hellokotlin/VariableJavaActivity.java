77b6bfc9e37f101c78fd28ab37a499c76712c239
H-E Yang <201921071@sangmyung.kr>
2021-04-29 15:42:12 +0900

    Time elapsed

--- app/src/main/java/com/example/hellokotlin/VariableJavaActivity.java
+++ app/src/main/java/com/example/hellokotlin/VariableJavaActivity.java
@@ -12,9 +12,10 @@ import java.util.Locale;
 
 public class VariableJavaActivity extends AppCompatActivity {
     int clickCount;
-    TextView txtActivityStartTime, txtCountBtnClicks;
+    TextView txtActivityStartTime, txtCountBtnClicks, txtElapsedTime;
     Button btnClickMe;
     final long startTime = System.currentTimeMillis();
+    long elapsedSeconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +25,19 @@ public class VariableJavaActivity extends AppCompatActivity {
         txtActivityStartTime = findViewById(R.id.txtActivityStartTime);
         txtCountBtnClicks = findViewById(R.id.txtCountBtnClicks);
         btnClickMe = findViewById(R.id.btnClickMe);
+        txtElapsedTime = findViewById(R.id.txtElapsedTime);
 
         clickCount = 0;
 //        startTime = System.currentTimeMillis();
+
 
         btnClickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clickCount++;
                 txtCountBtnClicks.setText("Button clicks = "+ clickCount);
+                elapsedSeconds = (long) ((System.currentTimeMillis() - startTime)/1000.0);
+                txtElapsedTime.setText(elapsedSeconds +"seconds elapsed");
             }
         });
         String sTimeStamp = new SimpleDateFormat("HH:mm:ss", Locale.KOREA).format(startTime);
--- app/src/main/java/com/example/hellokotlin/VariableKotlinActivity.kt
+++ app/src/main/java/com/example/hellokotlin/VariableKotlinActivity.kt
@@ -15,15 +15,19 @@ class VariableKotlinActivity : AppCompatActivity() {
         val txtActivityStartTime = findViewById<TextView>(R.id.txtActivityStartTime)
         val txtCountBtnClicks = findViewById<TextView>(R.id.txtCountBtnClicks)
         val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+        val txtElapsedTime = findViewById<TextView>(R.id.txtElapsedTime)
 
         var clickCount = 0
         val startTime = System.currentTimeMillis()
         val sTimeStamp = SimpleDateFormat("HH:mm:ss", Locale.KOREA).format(startTime)
         txtActivityStartTime.text= "Activity start time = ${sTimeStamp}"
 
+
         btnClickMe.setOnClickListener{
             clickCount++
             txtCountBtnClicks.text = "Button clicks = ${clickCount}"
+            var elapesdSeconds : Long = ((System.currentTimeMillis() - startTime) / 1000.0).toLong()
+            txtElapsedTime.text = "${elapesdSeconds} seconds elapsed "
         }
     }
 }
